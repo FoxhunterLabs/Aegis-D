aa9c77168a1019274c4252eaaddb8837cc4d6c43
Foxhunter Labs <joewells636@gmail.com>
2025-12-27 21:16:57 -0500

    Initialize geometry module with vectors and cpa

---
+++ aegis_d/geometry/__init__.py
@@ -0,0 +1,21 @@
+from .vectors import (
+    norm,
+    unit,
+    dot,
+    relative_position,
+    relative_velocity,
+)
+from .cpa import (
+    time_to_closest_point_of_approach,
+    distance_at_cpa,
+)
+
+__all__ = [
+    "norm",
+    "unit",
+    "dot",
+    "relative_position",
+    "relative_velocity",
+    "time_to_closest_point_of_approach",
+    "distance_at_cpa",
+]
